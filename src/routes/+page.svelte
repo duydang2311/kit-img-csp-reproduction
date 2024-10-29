d873d1679dbf351818b4a9f8f0b9734caadb76fe
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-10-29 16:56:44 +0700

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import Img from "./Img.svelte";
+</script>
+
+<Img alt="" src="https://picsum.photos/200/300" class="bg-red-500" />
---
+++ src/routes/Img.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  const { ...props }: any = $props();
+</script>
+
+<img {...props} />
